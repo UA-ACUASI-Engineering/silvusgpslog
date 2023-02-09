075d22f762b78b87fbf3deb9c86c72535d3b48f6
Zach Barnes <zachbarnes622@gmail.com>
2023-02-09 07:42:46 -0900

    This is after adding multiple logging to the file.

--- test.py
+++ test.py
@@ -4,14 +4,35 @@ import select
 
 PORT = ':2947'
 
+def multGPSListen(silvus_ip1, silvus_ip2, silvus_ip3):
+    silvus_ip1 = silvus_ip1+PORT
+    silvus_ip2 = silvus_ip2+PORT
+    silvus_ip3 = silvus_ip3+PORT
+
+    timeout = 0.5
+
+    gpspipes = [subprocess.Popen(['gpspipe', '-r', silvus_ip1], stdout=subprocess.PIPE),
+                subprocess.Popen(['gpspipe', '-r', silvus_ip2], stdout=subprocess.PIPE),
+                subprocess.Popen(['gpspipe', '-r', silvus_ip3], stdout=subprocess.PIPE),]
+
+    while True:
+
+        ready, _, _ = select.select([gpspipe.stdout for gpspipe in gpspipes], [], [], timeout)
+
+        for pipe in ready:
+            for gpspipe in gpspipes:
+                if pipe == gpspipe.stdout:
+                    output = gpspipe.stdout.readline().decode()
+                    if '$GPGGA' in output:
+                        print(output)
+
 def gpsListen(silvus_ip):
     timeout = 0.5
 
     silvus_ip = silvus_ip + PORT
 
     while True:
-        process = subprocess.Popen(
-            ['gpspipe', '-r', silvus_ip], stdout=subprocess.PIPE)
+        process = subprocess.Popen(['gpspipe', '-r', silvus_ip], stdout=subprocess.PIPE)
         while True:
             ready, _, _ = select.select([process.stdout], [], [], timeout)
 
@@ -32,6 +53,9 @@ def gpsListen(silvus_ip):
 if __name__ =="__main__":
     silvus_ip1 = '172.20.81.233'
     silvus_ip2 = '172.20.125.84'
+    silvus_ip3 = '172.20.153.102'
 
-    gpsListen(silvus_ip1)
-    gpsListen(silvus_ip2)
+    multGPSListen(silvus_ip1, silvus_ip2, silvus_ip3)
+
+    #gpsListen(silvus_ip1)
+    #gpsListen(silvus_ip2)
