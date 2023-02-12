ddd8b6030f14da17acd98bfc1049f6a5feb84b0c
Zach Barnes <zachbarnes622@gmail.com>
2023-02-12 14:14:47 -0900

    did work on multiple simultaneous monitoring and tcp based error catching

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import select
+import socket
 
 PORT = ':2947'
 
@@ -38,9 +39,11 @@ def gpsListen(silvus_ip):
 
             if ready:
                 output = process.stdout.readline().decode()
-                if output == '' and process.poll() is None:
+                if output == []:
+                    print("Timeout occurred, restarting gpspipe")
                     break
                 if '$GPGGA' in output:
+                    print(output)
                     with open('output.txt', 'a') as file:
                         file.write(output)
 
@@ -49,13 +52,36 @@ def gpsListen(silvus_ip):
             print("No data received, restarting gpspipe")
         time.sleep(0.1)
 
+def gpsListenTimeout(silvus_ip):
+    timeout = 10
+
+    while True:
+        process = subprocess.Popen(['gpspipe', '-r', silvus_ip], stdout=subprocess.PIPE)
+
+        while True:
+            output = process.stdout.readline().decode()
+            if '$GPGGA' in output:
+                print(output)
+
+            try:
+                s = socket.create_connection((silvus_ip, int(PORT[1:])), timeout) # the port for socket must be passed as an integer. int(PORT[1:]) removes the colon from the beginning of PORT (taking advantage of the fact that PORT is a string) and then casts it to an int
+
+            except socket.timeout:
+                print("No connection to radio, restarting GPSpipe")
+                break
+            except socket.error as e:
+                print(f"Error connecting to {silvus_ip}: {e}")
+                continue
+
+
+            s.close()
 
 if __name__ =="__main__":
     silvus_ip1 = '172.20.81.233'
     silvus_ip2 = '172.20.125.84'
     silvus_ip3 = '172.20.153.102'
 
-    multGPSListen(silvus_ip1, silvus_ip2, silvus_ip3)
+    #multGPSListen(silvus_ip1, silvus_ip2, silvus_ip3)
 
-    #gpsListen(silvus_ip1)
+    gpsListenTimeout(silvus_ip1)
     #gpsListen(silvus_ip2)
